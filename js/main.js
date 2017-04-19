ceeffd7edca5f838447e720ad8f774172f9657c4
Elizabeth Ji <elizabethji@gmail.com>
2017-04-19 06:14:45 -0400

    Add rental map, modify rental data to include coordinates

--- js/main.js
+++ js/main.js
@@ -20,6 +20,13 @@ var EARNINGS_ID = '#bar-chart--earnings';
 var EARNINGS_CSV = '/data/recent-grads.csv';
 var EARNINGS_SVG = d3.select(EARNINGS_ID);
 
+var RENT_ID = '#map--rent';
+var RENT_CSV = '/data/rental-data.csv';
+var RENT_SVG = d3.select(RENT_ID);
+
+var CITIES = '/cities.csv';
+var TOOLTIP;
+
 function asCurrency(amt) {
   return '$' + Intl.NumberFormat().format(amt);
 }
@@ -77,8 +84,130 @@ function earningsByMajor(earnings_data) {
       .attr('width', (d) => x(d.median))
 }
 
+
+function drawMap(earnings_data){
+  // ----- SVG Setup ----------------------------------------------------------
+  var rect = RENT_SVG.node().getBoundingClientRect();
+  
+  var projection = d3.geoAlbersUsa()
+      .translate([500,250])
+          .scale([1000]);
+
+  var path = d3.geoPath().projection(projection);
+  var g = RENT_SVG.append('g');
+  RENT_SVG.attr('viewBox', '0 0 ' + rect.width + ' ' + (rect.height*1.05));
+
+  // ----- Map Drawing --------------------------------------------------------
+  d3.json("https://raw.githubusercontent.com/alignedleft/d3-book/master/chapter_12/us-states.json", function(error, us) {
+   if (error) throw error;
+
+   g.attr("class", "states")
+    .selectAll("path")
+               .data(us.features)
+               .enter()
+               .append("path")
+               .attr("d", path);
+  });
+
+  // TODO(liza8bit) Allow for selection of major category
+  var curCategory = SCIENCE;
+  var data = earnings_data.filter((d) => d.major_category === curCategory)
+    .map((d) => { return {major: d.major, median: d.median}; })
+    .sort((d1, d2) => d1.median - d2.median)
+  var majorOptions = document.getElementById('map--select-major').options;
+  data.forEach( (d) => majorOptions.add(new Option(d.major, d.major)));
+  var cities = RENT_SVG.append("g").attr("class", "cities");
+  TOOLTIP = d3.select("body")
+    .append("div")   
+    .attr("class", "tooltip")               
+    .style("opacity", 0);
+}
+
+function rentalPrices(rental_data, earnings_data){
+  var data = rental_data;
+  var projection = d3.geoAlbersUsa()
+    .translate([500,250])
+    .scale([1000]);
+  var sel = document.getElementById('map--select-top');
+  var sel_data = function(){
+    switch(sel.options[sel.selectedIndex].value){
+        case '0':
+          return data.slice(0,50);
+          break;
+        case '1':
+          return data.sort((d1, d2) => d2.rent - d1.rent).slice(0,50);
+          break;
+        case '2':
+          return data.sort((d1, d2) => d1.rent - d2.rent).slice(0,50); 
+          break;
+    }};
+
+  var circles = RENT_SVG.select("g.cities").selectAll("circle")
+    .data(sel_data, function(d){return d.RegionName});
+
+  circles.exit()
+    .transition()
+    .duration(500)
+    .style("opacity", 0)
+    .remove();
+
+  circles.enter()
+    .append("circle")
+    .attr("class", "city")
+    .attr("cx", function(d) {
+      return projection([d.lon, d.lat])[0];
+    })
+    .attr("cy", function(d) {
+      return projection([d.lon, d.lat])[1];
+    })
+    .on("mouseover", function(d) {      
+      TOOLTIP.transition()        
+        .duration(200)      
+        .style("opacity", .9);
+
+      TOOLTIP.text(d.RegionName+"\n Median Rent: "+asCurrency(d.rent))
+        .style("left", (d3.event.pageX) + "px")     
+        .style("top", (d3.event.pageY - 28) + "px");
+      this.parentNode.appendChild(this);
+    })
+    .on("mouseout", function(d) {       
+      TOOLTIP.transition()        
+        .duration(500)      
+        .style("opacity", 0);   
+    })
+    .transition()
+    .duration(500)
+    .attr("r", function(d) {
+      return Math.sqrt(d.rent/6);
+      })
+    .merge(circles);
+}
+
+function updateSelection(){
+  d3.csv(EARNINGS_CSV, function(error, all_data) {
+    if (error) throw error;
+
+    earningsByMajor(all_data);
+
+    d3.csv(RENT_CSV, function(error, rent_data) {
+      if (error) throw error;
+      rentalPrices(rent_data, all_data);
+    });
+  });
+}
+
+
+
 d3.csv(EARNINGS_CSV, function(error, all_data) {
   if (error) throw error;
 
   earningsByMajor(all_data);
+  drawMap(all_data);
+
+  d3.csv(RENT_CSV, function(error, rent_data) {
+    if (error) throw error;
+
+    rentalPrices(rent_data, all_data);
+  });
 });
+
